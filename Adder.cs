c1d1587586753b8fe8573a70add2ba640db4b825
Kevin Bost <kitokeboo@gmail.com>
2024-11-03 21:36:39 -0800

    Adding overloads example

--- Adder.cs
+++ Adder.cs
@@ -1,8 +1,17 @@
+using System.Runtime.CompilerServices;
+
 public static class Adder
 {
     public static int Add(int a, int b)
     {
         Console.WriteLine("adding integers");
+        return a + b;
+    }
+
+    [OverloadResolutionPriority(1)]
+    public static double Add(double a, double b)
+    {
+        Console.WriteLine("adding doubles");
         return a + b;
     }
 }
