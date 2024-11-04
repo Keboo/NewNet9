70d2573aa6d831e2dc3a0c2e05e0f3f413653480
Kevin Bost <kitokeboo@gmail.com>
2024-11-03 20:16:34 -0800

    Adding params array example

---
+++ ParamsTests.cs
@@ -0,0 +1,28 @@
+﻿namespace NewNet9;
+
+public class ParamsTests
+{
+    [Fact]
+    public void CanJoinStrings()
+    {
+
+        Assert.Equal("Hello World", Join("Hello", "World"));
+    }
+
+    [Fact]
+    public void CanJoinStringsFromList()
+    {
+        var strings = GetStrings();
+        Assert.Equal("Hello World", Join(strings.ToArray()));
+    }
+
+    private static List<string> GetStrings()
+    {
+        return ["Hello", "World"];
+    }
+
+    private static string Join(params string[] strings)
+    {
+        return string.Join(" ", strings);
+    }
+}
