5eae93b9a02e8764e41b03cc161fbc96a3752983
Kevin Bost <kitokeboo@gmail.com>
2024-11-04 08:26:05 -0800

    Adding threading sample

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿IEnumerable<Task<string>> tasks = Enumerable.Range(1, 10).Select(GetValue);
+
+await foreach(Task<string> task in Task.WhenEach(tasks))
+{
+    Console.WriteLine(task.Result);
+}
+
+async static Task<string> GetValue(int value)
+{
+    var delay = Random.Shared.Next(1, 5000);
+    await Task.Delay(delay);
+    return $"Done {value}";
+} 
