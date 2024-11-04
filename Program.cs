f8886e939d803e005c1dcad6b8283b9760fe1235
Kevin Bost <kitokeboo@gmail.com>
2024-11-03 20:52:22 -0800

    Ending escape example

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
-// \u001b is the unicode escape sequence for the escape character
+﻿// \u001b is the unicode escape sequence for the escape character
+// \e is the new C# 13 escape character, it is equivelent to \u001b
 Console.WriteLine("this is regular text");
-Console.WriteLine("\u001b[1mThis is a bold text\u001b[0m");
-Console.WriteLine("\u001b[4mThis is an underlined text\u001b[0m");
-Console.WriteLine("\u001b[32mThis text is green\u001b[0m");
+Console.WriteLine("\e[1mThis is a bold text\e[0m");
+Console.WriteLine("\e[4mThis is an underlined text\e[0m");
+Console.WriteLine("\e[32mThis text is green\e[0m");
