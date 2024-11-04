abc4946c8632c7cb24c6e2e59f198f50fa99e4ca
Kevin Bost <kitokeboo@gmail.com>
2024-11-04 07:15:19 -0800

    Adding regex sample

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.RegularExpressions;
+
+string input = "This is a sample sentence, with punctuation.";
+
+string[] words = MyRegex.WordSplitter().Split(input);
+
+foreach (string word in words)
+{
+    Console.WriteLine(word);
+}
+
+public partial class MyRegex
+{
+    [GeneratedRegex(@"\W+")]
+    public static partial Regex WordSplitter();
+}
