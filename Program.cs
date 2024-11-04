2767388ce9463ee605cebf17f784d199da4ce893
Kevin Bost <kitokeboo@gmail.com>
2024-11-03 22:22:04 -0800

    Adding TimeSpan example

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿
+TimeSpan timeSpan1 = TimeSpan.FromSeconds(value: 101.832);
+Console.WriteLine($"timeSpan1 = {timeSpan1}");
+
+TimeSpan timeSpan2 = TimeSpan.FromSeconds(value: 101) + TimeSpan.FromMilliseconds(832);
+Console.WriteLine($"timeSpan2 = {timeSpan2}");
