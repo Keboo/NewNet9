4f0f2b79103a25a11f10af3d93ab2e76305fb198
Kevin Bost <kitokeboo@gmail.com>
2024-11-03 22:05:18 -0800

    Json schema example

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Text.Json.Schema;
+
+JsonSerializerOptions options = JsonSerializerOptions.Default;
+JsonNode schema = options.GetJsonSchemaAsNode(typeof(Person));
+Console.WriteLine(schema.ToString());
+
+public record Person(string Name, int Age);
