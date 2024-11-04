f7a920b1f00e5029741efaf734bf3da5da57aacb
Kevin Bost <kitokeboo@gmail.com>
2024-11-03 21:27:28 -0800

    Indexer ending example

--- Indexer.cs
+++ Indexer.cs
@@ -10,16 +10,16 @@ public class Indexer
             Dates =
             {
                 //NB: DateTime's constructor is year, month, day
-                [0] = new DateTime(1492, 10, 12),
-                [1] = new DateTime(1776, 7, 4),
-                [2] = new DateTime(1789, 7, 14),
-                [3] = new DateTime(1861, 4, 12),
-                [4] = new DateTime(1865, 8, 9),
-                [5] = new DateTime(1950, 6, 25),
-                [6] = new DateTime(1944, 6, 6),
-                [7] = new DateTime(1969, 7, 20),
-                [8] = new DateTime(1989, 11, 9),
-                [9] = DateTime.Now
+                [^1] = new DateTime(1492, 10, 12),
+                [^2] = new DateTime(1776, 7, 4),
+                [^3] = new DateTime(1789, 7, 14),
+                [^4] = new DateTime(1861, 4, 12),
+                [^5] = new DateTime(1865, 8, 9),
+                [^6] = new DateTime(1950, 6, 25),
+                [^7] = new DateTime(1944, 6, 6),
+                [^8] = new DateTime(1969, 7, 20),
+                [^9] = new DateTime(1989, 11, 9),
+                [^10] = DateTime.Now
             }
         };
     }
