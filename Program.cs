361ceb8656c4ed0f10a280a6b8b75231bb8867f3
Kevin Bost <kitokeboo@gmail.com>
2024-11-03 20:50:37 -0800

    Console example

---
+++ Program.cs
@@ -0,0 +1,5 @@
+// \u001b is the unicode escape sequence for the escape character
+Console.WriteLine("this is regular text");
+Console.WriteLine("\u001b[1mThis is a bold text\u001b[0m");
+Console.WriteLine("\u001b[4mThis is an underlined text\u001b[0m");
+Console.WriteLine("\u001b[32mThis text is green\u001b[0m");
