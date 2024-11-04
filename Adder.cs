9c4a4c799a07e04c063ee27b3b0d79df1e91c466
Kevin Bost <kitokeboo@gmail.com>
2024-11-03 21:35:21 -0800

    Adding overload example

---
+++ Adder.cs
@@ -0,0 +1,8 @@
+public static class Adder
+{
+    public static int Add(int a, int b)
+    {
+        Console.WriteLine("adding integers");
+        return a + b;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,2 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine($"Result {Adder.Add(40, 2)}");
