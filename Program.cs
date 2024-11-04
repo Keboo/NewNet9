4c9797d120d92f7691291d3e806c264d0ce37d14
Kevin Bost <kitokeboo@gmail.com>
2024-11-04 07:49:10 -0800

    json-serialization

--- Program.cs
+++ Program.cs
@@ -6,11 +6,56 @@ using System.Text.Json.Serialization;
 
 JsonSerializerOptions options = new(JsonSerializerOptions.Default)
 {
-    PropertyNamingPolicy = JsonNamingPolicy.KebabCaseUpper,
+    PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
     NumberHandling = JsonNumberHandling.WriteAsString,
     UnmappedMemberHandling = JsonUnmappedMemberHandling.Disallow,
-};
-JsonNode schema = options.GetJsonSchemaAsNode(typeof(Person));
-Console.WriteLine(schema.ToString());
 
-public record Person(string Name, int Age);
+    RespectNullableAnnotations = true,
+
+    RespectRequiredConstructorParameters = true,
+
+    WriteIndented = true,
+    IndentCharacter = ' ',
+    IndentSize = 14,
+};
+
+string json = JsonSerializer.Serialize(
+    new Person(25) { FirstName = "John", LastName = "Doe", Job = Job.Developer },
+    options
+    );
+Console.WriteLine(json);
+
+//NB: Missing "age" and valid vlaue for "lastName"
+Person person = JsonSerializer.Deserialize<Person>(
+    $$"""
+    {
+        "firstName": "John",
+        "lastName": null,
+        "job": 3
+    }
+    """, options);
+
+Console.WriteLine(person);
+
+public record Person(int Age)
+{
+    public string? FirstName { get; init; }
+    public required string LastName { get; init; }
+    public Job Job { get; init; } = Job.Developer;
+}
+
+[JsonConverter(typeof(JsonStringEnumConverter))]
+public enum Job
+{
+    Unknown,
+    [JsonStringEnumMemberName("bug-whisperer")]
+    Developer,
+    [JsonStringEnumMemberName("it-manager")]
+    DigitalOverlord,
+    [JsonStringEnumMemberName("creative-director")]
+    WizrdOfLightbulbMoments,
+    [JsonStringEnumMemberName("marketing-manager")]
+    AmbassadorOfBuzz,
+    [JsonStringEnumMemberName("graphic-designer")]
+    PixelSorcerer 
+}
